dbdfe3da8321bf5de7689f8d0bb0eb1aaddd983a
Nidhi Tailor <51026640+tailornp@users.noreply.github.com>
2025-08-30 00:49:24 +0000

    added datasource class for db connection

---
+++ src/main/java/bank/DataSource.java
@@ -0,0 +1,69 @@
+package bank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DataSource {
+  public static Connection connect(){
+    String db_file = "jdbc:sqlite:resources/bank.db";
+    Connection connection = null;
+    try{
+     connection = DriverManager.getConnection(db_file);
+     System.out.println("We are connected");
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+    return connection;
+  }
+
+  public static Customer getCustomer(String username){
+    String sql= "select * from customers where username = ?";
+    Customer customer = null;
+    try(Connection connection = connect();
+    PreparedStatement statement = connection.prepareStatement(sql)){
+      statement.setString(1, username);
+      try(ResultSet resultSet = statement.executeQuery()){
+        customer = new Customer(
+          resultSet.getInt("id"), 
+          resultSet.getString("name"),
+          resultSet.getString("username"), 
+          resultSet.getString("password"), 
+          resultSet.getInt("account_id"));
+      }
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
+
+  public static Account getAccount(int id){
+    String sql = "select * from accounts where id=?";
+    Account account = null;
+    try(Connection connection = connect();
+    PreparedStatement statement = connection.prepareStatement(sql)){
+      statement.setInt(1, id);
+      try(ResultSet resultSet = statement.executeQuery()){
+        account = new Account(
+          resultSet.getInt("id"),
+          resultSet.getString("type"), 
+          resultSet.getDouble("balance"));
+      }
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+
+    return account;
+  }
+
+  public static void main(String[] args) {
+   Customer customer = getCustomer("clillea8@nasa.gov");
+   System.out.println(customer.getName());
+
+   Account account = getAccount(10385);
+   System.out.println(account.getBalance());
+  }
+}
